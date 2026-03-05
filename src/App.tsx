96669f09cfe3dfd67b7f9462b8d16304c19c17a6
= <Athena.Whiteley@apexon.com>
2026-03-05 12:44:41 +0000

    Working app before new img testing

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ function App() {
   const [nextAnswer, setNextAnswer] = useState("")
   const [question, setQuestion] = useState("")
   const [shake, setShake] = useState(false)
+  const [isBallOpen, setBallOpen] = useState(false)
 
   let options = ['Yes', 'No', 'Maybe..', 'Definitely not']
 
   const buttonHandler = () => {
     if (!shake && question.includes("?") && question.length>1){
       setShake(true) 
+      setBallOpen(false);
       const randomOptionIndex = Math.floor(Math.random()*options.length) 
       const randomOption = options[randomOptionIndex]
       setAnswer(randomOption)
@@ -29,7 +31,13 @@ function App() {
     <>
     <div id="appBody">
       <h1>Welcome to magic 8 ball</h1>
-      <img src={ball} alt="" id="ball" onAnimationEnd={()=>setShake(false)} className={shake? "shakeIMG":""}/>
+      <div className='imageHolder'>
+        <img  src={isBallOpen?ballOpen:ballClosed} alt="" id="ball"
+        onAnimationEnd={()=>{setShake(false); setBallOpen(true)}} 
+        className={(!isBallOpen)?"shakeIMG":"openBall"}
+        />
+      </div>
+
       <br />
       <div className="inputField">
           <input type="text" id="question" placeholder='Enter a question...' value={question} onChange={(e)=>setQuestion(e.target.value)}/>
