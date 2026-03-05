a0e1369ded4efb68ad3e8bcf8b303de5b6c2cc54
= <Athena.Whiteley@apexon.com>
2026-03-05 09:18:04 +0000

    Rearranging app

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [answer, setAnswer] = useState("")
+  const [nextAnswer, setNextAnswer] = useState("")
+  const [question, setQuestion] = useState("")
+  const [shake, setShake] = useState(false)
+
+  let options = ['Yes', 'No', 'Maybe..', 'Definitely not']
+
+  const buttonHandler = () => {
+    if (!shake && question.includes("?") && question.length>1){
+      setShake(true) 
+      const randomOptionIndex = Math.floor(Math.random()*options.length) 
+      const randomOption = options[randomOptionIndex]
+      setAnswer(randomOption)
+      setTimeout(()=>setNextAnswer(answer), 3000)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Welcome to magic 8 ball</h1>
+      <img src="" alt="" id="ball" onAnimationEnd={()=>setShake(false)} />
+      <button onClick={buttonHandler} disabled={shake}>Submit question</button>
+      <input type="text" placeholder='Enter a question...' value={question} onChange={(e)=>setQuestion(e.target.value.trim())}/>
+      <p>{nextAnswer}</p>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './App'
 import Header from './Header'
 import Image from './Image'
 import QuestionHandler from './Question'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Header />
-    <Image/>
-    <QuestionHandler/>
+    <App/>
   </StrictMode>,
 )
