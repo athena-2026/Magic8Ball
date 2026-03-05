a011805bee823c9b146c2a1f32f20634b95048e2
= <Athena.Whiteley@apexon.com>
2026-03-05 09:46:12 +0000

    Working shaking and debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import ball from './assets/ball.png'
 import './App.css'
 
 
@@ -17,17 +18,21 @@ function App() {
       const randomOptionIndex = Math.floor(Math.random()*options.length) 
       const randomOption = options[randomOptionIndex]
       setAnswer(randomOption)
+      console.log(answer)
       setTimeout(()=>setNextAnswer(answer), 3000)
     }
   }
 
   return (
     <>
+    <div id="appBody">
       <h1>Welcome to magic 8 ball</h1>
-      <img src="" alt="" id="ball" onAnimationEnd={()=>setShake(false)} className={shake? "shake":""}/>
-      <button onClick={buttonHandler} disabled={shake} >Submit question</button>
+      <img src={ball} alt="" id="ball" onAnimationEnd={()=>setShake(false)} className={shake? "shakeIMG":""}/>
+      <br />
       <input type="text" placeholder='Enter a question...' value={question} onChange={(e)=>setQuestion(e.target.value.trim())}/>
+      <button onClick={buttonHandler} disabled={shake} >Submit question</button>
       <p>{nextAnswer}</p>
+      </div>
     </>
   )
 }
