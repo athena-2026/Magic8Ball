16bcd383a799b290b0db371a793fea98ba655c68
= <Athena.Whiteley@apexon.com>
2026-03-19 10:19:14 +0000

    Adding integration testing between input field component and image holder component

--- testing_folder/uiTesting.test.tsx
+++ testing_folder/uiTesting.test.tsx
@@ -3,6 +3,7 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 import InputField from '../src/Components/InputField';
+import ImageHolder from '../src/Components/ImageHolder'
 
 import {AppProviders} from'../src/Context/ContextProvider'
 
@@ -21,7 +22,6 @@ describe('Input Field component', ()=> {
 
         fireEvent.click(button)
 
-        const text = screen.getByRole("textbox").textContent
         expect(button.hasAttribute('disabled')).toBe(false)
     })
     test ('input 1 character or below should not be accepted', () => {
@@ -36,7 +36,6 @@ describe('Input Field component', ()=> {
 
         fireEvent.click(button)
 
-        const text = screen.getByRole("textbox").textContent
         expect(button.hasAttribute('disabled')).toBe(false)
     })
     test ('input with ? and length above 1 should be accepted', () => {
@@ -51,11 +50,47 @@ describe('Input Field component', ()=> {
 
         fireEvent.click(button)
 
-        const text = screen.getByRole("textbox").textContent
-        expect(button.hasAttribute('disabled')).toBe(true)
+        setTimeout(()=>{
+            expect(button.hasAttribute('disabled')).toBe(true)
+        }, 1000)
+
     })
 })
 
 describe('Integration testing between input field component and image holder component', ()=> {
-    test('')
+    test('answer should be cleared/empty before displaying random answer', ()=> {
+        render(
+        <AppProviders>
+            <ImageHolder/>
+            <InputField/>
+        </AppProviders>)
+        const button = screen.getByRole("button", {name:"Submit question"})
+        const inputField = screen.getByRole("textbox")
+        inputField.textContent = "Will this test work?"
+
+        fireEvent.click(button)
+        
+        const answer = screen.getByTestId("answer")
+        expect(answer.textContent).toBe('')
+    })
+
+        test('answer should display a random phrase 2 seconds after button is clicked', ()=> {
+        render(
+        <AppProviders>
+            <ImageHolder/>
+            <InputField/>
+        </AppProviders>)
+
+        let options = ['Maybe..', 'Not too sure..', 'Possibly..', 'I think so', 'Ask me another time..', 'No'];
+        const button = screen.getByRole("button", {name:"Submit question"})
+        const inputField = screen.getByRole("textbox")
+        inputField.textContent = "Will this test work?"
+
+        fireEvent.click(button)
+        
+        setTimeout( ()=> {
+            expect(options).toContain(screen.getByTestId("answer").textContent)
+        }, 2000)
+        
+    })
 })
