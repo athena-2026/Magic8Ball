a15270b7db5818966c77912b25732ffa9a8a6db9
= <Athena.Whiteley@apexon.com>
2026-03-24 09:26:04 +0000

    Editing counter to fit screen and starting to make a navbar

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -5,7 +5,7 @@ function Counter() {
   const {counter} = useCounter()
   return (
     <>
-      <p className='counter'>You have asked {counter} questions</p>
+    <p className='counter'>You have asked {counter} question(s)</p>
     </>
   )
 }
